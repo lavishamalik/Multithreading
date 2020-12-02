871c3f07f48f235c5e6286dce0a4f9fec912d201
Lavisha <lavishamalik1@gmail.com>
2020-12-02 19:10:58 +0530

    First commit

---
+++ app/src/main/java/com/example/mclabexp5/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.mclabexp5;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+    ImageView img;
+    Button bt1,bt2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt1 = findViewById(R.id.button);
+        bt2=  findViewById(R.id.button2);
+        img = findViewById(R.id.imageView);
+
+        bt1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                new Thread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        img.post(new Runnable()
+                        {
+                            @Override
+                            public void run()
+                            {
+                                img.setImageResource(R.drawable.image1);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+
+        bt2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                new Thread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        img.post(new Runnable()
+                        {
+                            @Override
+                            public void run()
+                            {
+                                img.setImageResource(R.drawable.image2);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+    }
+}
